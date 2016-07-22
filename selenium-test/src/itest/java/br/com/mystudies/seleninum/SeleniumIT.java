ccb1e791a901767d1ac02fe21028574760cd9552
Robso Duarte <robson.o.d@gmail.com>
2016-07-21 21:47:49 -0300

    add new src folder to integration tests.

---
+++ selenium-test/src/itest/java/br/com/mystudies/seleninum/SeleniumIT.java
@@ -0,0 +1,72 @@
+package br.com.mystudies.seleninum;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.thoughtworks.selenium.Selenium;
+
+
+public class SeleniumIT {
+
+	private Selenium selenium;
+
+
+
+
+	@Before
+	public void setUp() throws Exception {
+	/*	WebDriver driver = new PhantomJSDriver(temp());
+		String baseUrl = "http://localhost:8080/selenium-test/";
+		selenium = new WebDriverBackedSelenium(driver, baseUrl);
+*/	}
+
+
+
+
+
+
+
+
+
+
+	@After
+	public void tearDown() throws Exception {
+		/*selenium.stop();*/
+	}
+
+
+
+
+
+
+
+	@Test
+	public void testSelinum() throws Exception {
+	/*	selenium.open("/selenium-test/");
+		selenium.type("id=name", "Robson");
+		selenium.type("id=password", "Ana");
+		selenium.click("css=input[type=\"submit\"]");
+		selenium.waitForPageToLoad("30000");
+
+		assertThat(selenium.getBodyText(), equalTo("Welcome Robson"));*/
+	}
+
+
+
+
+
+
+
+/*	private DesiredCapabilities temp() {
+		DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+		desiredCapabilities.setCapability(PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY, "E:/apps/phantomjs-2.1.1-windows/bin/phantomjs.exe");
+		return desiredCapabilities;
+	}*/
+
+
+
+
+
+
+}

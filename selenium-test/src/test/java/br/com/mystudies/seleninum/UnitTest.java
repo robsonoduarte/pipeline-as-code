7670e61aec61d6b41896b600e0ac106d71b8d8bc
Robso Duarte <robson.o.d@gmail.com>
2016-07-29 23:47:59 -0300

    Rename the class.

---
+++ selenium-test/src/test/java/br/com/mystudies/seleninum/UnitTest.java
@@ -0,0 +1,20 @@
+package br.com.mystudies.seleninum;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+
+public class UnitTest {
+
+
+
+	@Test
+	public void unitTest() {
+		assertThat("unit test", equalTo("unit test"));
+	}
+
+
+
+
+}

1a104ad1b2b3048e0357bbcf10c0f2e0358f8d99
Robson Duarte <robson.o.d@gmail.com>
2016-10-12 19:44:14 -0300

    add TODO class.

---
+++ src/main/java/br/com/mystudies/seleninum/TODO.java
@@ -0,0 +1,11 @@
+package br.com.mystudies.seleninum;
+
+public class TODO {
+
+	// TODO: docker in docker network to access
+	// TODO: fix problems of navigation in spring security
+	// TODO: add simple home page
+	// TODO: task run image remote
+	// TODO: analyze the https://github.com/spring-guides/gs-securing-web/blob/master/complete/src/test/java/hello/ApplicationTests.java
+	// TODO: Selininum
+}

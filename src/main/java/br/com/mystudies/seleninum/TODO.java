ab192cbc6b8bb4faf580aafcc1058a5897cd978f
Robson Duarte <robson.o.d@gmail.com>
2017-01-09 21:35:33 -0200

    add new task

--- src/main/java/br/com/mystudies/seleninum/TODO.java
+++ src/main/java/br/com/mystudies/seleninum/TODO.java
@@ -3,6 +3,7 @@ package br.com.mystudies.seleninum;
 public class TODO {
 
 	// TODO: docker in docker network to access
+	// TODO: Unit test ( TDD ) AuthenticationProvider
 	// TODO: task run image remote
 	// TODO: analyze the https://github.com/spring-guides/gs-securing-web/blob/master/complete/src/test/java/hello/ApplicationTests.java
 	// TODO: Selininum
